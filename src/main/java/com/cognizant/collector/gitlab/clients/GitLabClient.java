8bb284e9804580f77991516b106109d95a0027d1
Shankar Mahadevan <Shankar.Mahadevan2@cognizant.com>
2021-03-18 16:43:20 +0530

    License and author update
     License and author update changes in all pages

--- src/main/java/com/cognizant/collector/gitlab/clients/GitLabClient.java
+++ src/main/java/com/cognizant/collector/gitlab/clients/GitLabClient.java
@@ -1,3 +1,21 @@
+
+/*
+ * © [2021] Cognizant. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	http:www.apache.orglicensesLICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 package com.cognizant.collector.gitlab.clients;
 
 import com.cognizant.collector.gitlab.beans.branches.GitLabBranch;
@@ -19,8 +37,10 @@ import java.net.URI;
 import java.util.List;
 import java.util.Map;
 
-/**
- * Created by 784420 on 10/11/2019 3:17 PM
+/*
+ * GitLabClient
+ *
+ * @author Cognizant
  */
 public interface GitLabClient {
 
--- src/main/java/com/cognizant/collector/gitlab/component/GitLabComponent.java
+++ src/main/java/com/cognizant/collector/gitlab/component/GitLabComponent.java
@@ -1,3 +1,21 @@
+
+/*
+ * © [2021] Cognizant. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	http:www.apache.orglicensesLICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 package com.cognizant.collector.gitlab.component;
 
 import com.cognizant.collector.gitlab.beans.branches.GitLabBranch;
@@ -19,8 +37,10 @@ import javax.annotation.PostConstruct;
 import java.io.IOException;
 import java.util.List;
 
-/**
- * Created by 784420 on 10/11/2019 3:36 PM
+/*
+ * GitLabComponent
+ *
+ * @author Cognizant
  */
 @Component
 @Slf4j
